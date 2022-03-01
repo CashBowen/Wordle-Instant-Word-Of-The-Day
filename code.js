7861d084b30451512e0a8e50afd7ed11222116c3
-Corgi- <99050972+Cor-gi@users.noreply.github.com>
2022-02-28 18:09:11 -0700

    Optimization

--- code.js
+++ code.js
@@ -1,37 +1,24 @@
+window.wordle.instant_word = function() {
+
 let WordOfTheDay = (new window.wordle.bundle.GameApp).solution;
 
 window.dispatchEvent(new KeyboardEvent('keydown', {
-
   'key': WordOfTheDay.charAt(0)
-
 }));
-
 window.dispatchEvent(new KeyboardEvent('keydown', {
-
   'key': WordOfTheDay.charAt(1)
-
 }));
-
 window.dispatchEvent(new KeyboardEvent('keydown', {
-
   'key': WordOfTheDay.charAt(2)
-
 }));
-
 window.dispatchEvent(new KeyboardEvent('keydown', {
-
   'key': WordOfTheDay.charAt(3)
-
 }));
-
 window.dispatchEvent(new KeyboardEvent('keydown', {
-
   'key': WordOfTheDay.charAt(4)
-
 })); 
-
 window.dispatchEvent(new KeyboardEvent('keydown', {
-
   'key': 'Enter'
-
 }));
+
+}
